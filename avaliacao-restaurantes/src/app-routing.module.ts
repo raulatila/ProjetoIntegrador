de41f11d425c7d3eab39126aa00c53a17f0b8971
Guilherme Honda <127107056+K9WHITE@users.noreply.github.com>
2025-05-05 23:45:25 +0000

    correcao de telas 3

--- avaliacao-restaurantes/src/app-routing.module.ts
+++ avaliacao-restaurantes/src/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './app/components/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
--- avaliacao-restaurantes/src/app.module.ts
+++ avaliacao-restaurantes/src/app.module.ts
@@ -7,9 +7,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './app/components/login/login.component';
 
 
 
